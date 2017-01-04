299307ab7fdedadad84911acc68d2e60c5312150
Hee <i@hee.moe>
2017-01-04 17:13:48 +0800

    add other page

---
+++ src/components/other/OtherPage.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hee on 1/4/17.
+ */
+import React, {Component} from 'react';
+class OtherPage extends Component {
+    render(){
+        return(
+            <p> other page </p>
+        );
+}
+}
+
+export default OtherPage;
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,12 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
-
+import OtherPage from './components/other/OtherPage';
 export default(
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
         <Route path="courses" component={CoursesPage}/>
         <Route path="about" component={AboutPage}/>
+        <Route path="other" component={OtherPage}/>
     </Route>
 );
